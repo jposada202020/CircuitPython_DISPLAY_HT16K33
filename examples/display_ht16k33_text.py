fb56f8ce34f054822783d5235c6a36cdb877d0f6
jposada202020 <jquintana202020@gmail.com>
2023-05-24 15:05:25 -0400

    adding text example

--- examples/display_ht16k33_text.py
+++ examples/display_ht16k33_text.py
@@ -8,23 +8,31 @@ import board
 import adafruit_framebuf
 from display_ht16k33 import matrix
 
-matrix = matrix.Matrix16x8()
+# Uncomment/Comment the following lines to select the size of your Matrix
+col = 16
+# col = 8
+
+if col == 16:
+    matrix = matrix.Matrix16x8()
+else:
+    matrix = matrix.Matrix8x8()
+
 display = board.DISPLAY
 display.show(matrix.group)
 
-buf = bytearray(16)
-text_to_show = "Hello :)"
-fb = adafruit_framebuf.FrameBuffer(buf, 16, 8, adafruit_framebuf.MVLSB)
 
+buf = bytearray(col)
+text_to_show = "Hello :)"
+fb = adafruit_framebuf.FrameBuffer(buf, col, 8, adafruit_framebuf.MVLSB)
 
 while True:
     for i in range(len(text_to_show) * 8):
         fb.fill(0)
-        fb.text(text_to_show, -i + 16, 0, color=1)
+        fb.text(text_to_show, -i + col, 0, color=1)
         matrix.fill(0)
-        for x in range(16):
+        for x in range(col):
             bite = buf[x]
             for y in range(8):
                 bit = 1 << y & bite
                 if bit:
-                    matrix[16 - x, y + 1] = 1
+                    matrix[col - x, y + 1] = 1
